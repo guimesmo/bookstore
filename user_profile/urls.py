0c8950e638a825fe858d49533a18c1be4e272459
Luiz Guilherme Silva <luiz.silva@dextra-sw.com>
2020-03-04 22:51:33 -0300

    Fixed urls.py

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,6 +4,6 @@ from rest_framework.routers import DefaultRouter
 from .views import UserProfileViewSet
 
 router = DefaultRouter()
-router.register(r'api/users/user', UserProfileViewSet, base_name='users')
+router.register(r'api/users/user', UserProfileViewSet)
 
 urlpatterns = router.urls
